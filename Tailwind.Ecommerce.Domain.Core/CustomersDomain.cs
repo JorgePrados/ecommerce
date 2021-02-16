55554032dd3cfb5abaee6f3d55a845abee8e935b
JorgePrados <45536931+JorgePrados@users.noreply.github.com>
2021-02-16 09:47:21 +0100

    Add files via upload

---
+++ Tailwind.Ecommerce.Domain.Core/CustomersDomain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Tailwind.Ecommerce.Domain.Entity;
+using Tailwind.Ecommerce.Domain.Interface;
+using Tailwind.Ecommerce.Infrastructure.Interface;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Tailwind.Ecommerce.Domain.Core
+{
+    public class CustomersDomain: ICustomersDomain
+    {
+        private readonly ICustomersRepository _customerRepository;
+
+        public CustomersDomain(ICustomersRepository customersRepository)
+        {
+            _customerRepository = customersRepository;
+        }
+
+        #region Métodos Síncronos
+        public bool Insert(Customers customers)
+        {
+            return _customerRepository.Insert(customers);
+        }
+        public bool Update(Customers customers)
+        {
+            return _customerRepository.Update(customers);
+        }
+        public bool Delete(string customerId)
+        {
+            return _customerRepository.Delete(customerId);
+        }
+        public Customers Get(string customerId)
+        {
+            return _customerRepository.Get(customerId);
+        }
+        public IEnumerable<Customers> GetAll()
+        {
+            return _customerRepository.GetAll();
+        }
+        #endregion
+
+        #region Métodos Asíncronos
+        public async Task<bool> InsertAsync(Customers customers)
+        {
+            return await _customerRepository.InsertAsync(customers);
+        }
+        public async Task<bool> UpdateAsync(Customers customers)
+        {
+            return await _customerRepository.UpdateAsync(customers);
+        }
+        public async Task<bool> DeleteAsync(string customerId)
+        {
+            return await _customerRepository.DeleteAsync(customerId);
+        }
+        public async Task<Customers> GetAsync(string customerId)
+        {
+            return await _customerRepository.GetAsync(customerId);
+        }
+        public async Task<IEnumerable<Customers>> GetAllAsync()
+        {
+            return await _customerRepository.GetAllAsync();
+        }
+        #endregion
+    }
+}
---
+++ Tailwind.Ecommerce.Infrastructure.Repository/CustomersRepository.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using Tailwind.Ecommerce.Domain.Entity;
+using Tailwind.Ecommerce.Infrastructure.Interface;
+using Tailwind.Ecommerce.Transversal.Common;
+using Dapper;
+using System.Data;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Tailwind.Ecommerce.Infrastructure.Repository
+{
+    public class CustomersRepository: ICustomersRepository
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        public CustomersRepository(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+        #region Métodos Sincronos
+        public bool Insert(Customers customers)
+        {
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                var query = "CustomersInsert";
+                var parameters = new DynamicParameters();
+                parameters.Add("CustomerId", customers.CustomerId);
+                parameters.Add("CompanyName", customers.CompanyName);
+                parameters.Add("ContactName", customers.ContactName);
+                parameters.Add("ContactTitle", customers.ContactTitle);
+                parameters.Add("Address", customers.Address);
+                parameters.Add("City", customers.City);
+                parameters.Add("Region", customers.Region);
+                parameters.Add("PostalCode", customers.PostalCode);
+                parameters.Add("Country", customers.Country);
+                parameters.Add("Phone", customers.Phone);
+                parameters.Add("Fax", customers.Fax);
+
+                var result = connection.Execute(query, param:parameters, commandType: CommandType.StoredProcedure);
+                return result > 0;
+            }
+        }
+
+        public bool Update(Customers customers)
+        {
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                var query = "CustomersUpdate";
+                var parameters = new DynamicParameters();
+                parameters.Add("CustomerId", customers.CustomerId);
+                parameters.Add("CompanyName", customers.CompanyName);
+                parameters.Add("ContactName", customers.ContactName);
+                parameters.Add("ContactTitle", customers.ContactTitle);
+                parameters.Add("Address", customers.Address);
+                parameters.Add("City", customers.City);
+                parameters.Add("Region", customers.Region);
+                parameters.Add("PostalCode", customers.PostalCode);
+                parameters.Add("Country", customers.Country);
+                parameters.Add("Phone", customers.Phone);
+                parameters.Add("Fax", customers.Fax);
+
+                var result = connection.Execute(query, param: parameters, commandType: CommandType.StoredProcedure);
+                return result > 0;
+            }
+        }
+
+        public bool Delete(String customerId)
+        {
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                var query = "CustomersDelete";
+                var parameters = new DynamicParameters();
+                parameters.Add("CustomerId", customerId);
+               
+
+                var result = connection.Execute(query, param: parameters, commandType: CommandType.StoredProcedure);
+                return result > 0;
+            }
+        }
+
+        public Customers Get(String customerId)
+        {
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                var query = "CustomersGetById";
+                var parameters = new DynamicParameters();
+                parameters.Add("CustomerId", customerId);
+
+
+                var customer = connection.QuerySingle<Customers>(query, param: parameters, commandType: CommandType.StoredProcedure);
+                return customer;
+            }
+        }
+
+        public IEnumerable<Customers> GetAll()
+        {
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                var query = "CustomersList";
+
+                var customers = connection.Query<Customers>(query, commandType: CommandType.StoredProcedure);
+                return customers;
+            }
+        }
+        #endregion
+
+        #region Métodos Asíncronos
+        public async Task<bool> InsertAsync(Customers customers)
+        {
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                var query = "CustomersInsert";
+                var parameters = new DynamicParameters();
+                parameters.Add("CustomerId", customers.CustomerId);
+                parameters.Add("CompanyName", customers.CompanyName);
+                parameters.Add("ContactName", customers.ContactName);
+                parameters.Add("ContactTitle", customers.ContactTitle);
+                parameters.Add("Address", customers.Address);
+                parameters.Add("City", customers.City);
+                parameters.Add("Region", customers.Region);
+                parameters.Add("PostalCode", customers.PostalCode);
+                parameters.Add("Country", customers.Country);
+                parameters.Add("Phone", customers.Phone);
+                parameters.Add("Fax", customers.Fax);
+
+                var result = await connection.ExecuteAsync(query, param: parameters, commandType: CommandType.StoredProcedure);
+                return result > 0;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(Customers customers)
+        {
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                var query = "CustomersUpdate";
+                var parameters = new DynamicParameters();
+                parameters.Add("CustomerId", customers.CustomerId);
+                parameters.Add("CompanyName", customers.CompanyName);
+                parameters.Add("ContactName", customers.ContactName);
+                parameters.Add("ContactTitle", customers.ContactTitle);
+                parameters.Add("Address", customers.Address);
+                parameters.Add("City", customers.City);
+                parameters.Add("Region", customers.Region);
+                parameters.Add("PostalCode", customers.PostalCode);
+                parameters.Add("Country", customers.Country);
+                parameters.Add("Phone", customers.Phone);
+                parameters.Add("Fax", customers.Fax);
+
+                var result = await connection.ExecuteAsync(query, param: parameters, commandType: CommandType.StoredProcedure);
+                return result > 0;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(String customerId)
+        {
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                var query = "CustomersDelete";
+                var parameters = new DynamicParameters();
+                parameters.Add("CustomerId", customerId);
+
+
+                var result = await connection.ExecuteAsync(query, param: parameters, commandType: CommandType.StoredProcedure);
+                return result > 0;
+            }
+        }
+
+        public async Task<Customers> GetAsync(String customerId)
+        {
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                var query = "CustomersGetById";
+                var parameters = new DynamicParameters();
+                parameters.Add("CustomerId", customerId);
+
+
+                var customer = await connection.QuerySingleAsync<Customers>(query, param: parameters, commandType: CommandType.StoredProcedure);
+                return customer;
+            }
+        }
+
+        public async Task<IEnumerable<Customers>> GetAllAsync()
+        {
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                var query = "CustomersList";
+
+                var customers = await connection.QueryAsync<Customers>(query, commandType: CommandType.StoredProcedure);
+                return customers;
+            }
+        }
+
+        #endregion
+    }
+}
