533039abbbbe3c5e39464e72e5dd9df739edaeaa
JorgePrados <45536931+JorgePrados@users.noreply.github.com>
2021-02-16 09:48:24 +0100

    Add files via upload

---
+++ Tailwind.Ecommerce.Transversal.Common/IConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Tailwind.Ecommerce.Transversal.Common
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection GetConnection { get; }
+    }
+}
---
+++ Tailwind.Ecommerce.Transversal.Mapper/MappingsProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AutoMapper;
+using Tailwind.Ecommerce.Domain.Entity;
+using Tailwind.Ecommerce.Application.DTO;
+namespace Tailwind.Ecommerce.Transversal.Mapper
+{
+    public class MappingsProfile : Profile
+    {
+        public MappingsProfile()
+        {
+            CreateMap<Customers, CustomersDto>().ReverseMap();
+
+            //CreateMap<Customers, CustomersDto>().ReverseMap()
+            //    .ForMember(destintation => destintation.CustomerId, source => source.MapFrom(src => src.CustomerId));
+        }
+    }
+}
